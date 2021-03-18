95f0129ceaa3e5850d4b6925a260b92f6741c02c
Trent Renden <trent_renden@hotmail.com>
2021-03-18 15:34:43 +1100

    Done with assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const add = function(a, b){
+    return a+b;
+}
+const subtract = function(a, b){
+    return a-b;
+}
+const multiply = function(a, b){
+    return a*b;
+}
+const divide = function(a, b){
+    return a/b;
+}
+const increment = function(n){
+    return ++n;
+}
+const decrement = function(n){
+    return --n;
+}
+const  makeInt = function(n){
+   parseInt (n, 10);
+    return parseInt (n, 10);
+}
+const preserveDecimal = function(n){
+    parseFloat (n);
+    return parseFloat (n);
+}
